06479fddfe7149fae36e8a98ebd3f64941976f9b
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-02-22 18:47:02 +0100

    chore: update rector to 1.0.1

--- rector.php
+++ rector.php
@@ -3,41 +3,36 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\FunctionLike\AddReturnTypeDeclarationFromYieldsRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::PHPUNIT_EXCEPTION,
-        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
-        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-    ]);
-
-    $rectorConfig->autoloadPaths([
-        __DIR__ . '/.Build/vendor/autoload.php',
-    ]);
-    $rectorConfig->importShortClasses(false);
-    $rectorConfig->importNames();
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/Classes',
         __DIR__ . '/Tests',
-    ]);
-    $rectorConfig->skip([
+    ])
+    ->withPhpSets()
+    ->withAutoloadPaths([
+        __DIR__ . '/.Build/vendor/autoload.php',
+    ])
+    ->withImportNames(
+        importShortClasses: false,
+        removeUnusedImports: true,
+    )
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        earlyReturn: true,
+    )
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+    ])
+    ->withRootFiles()
+    ->withSkip([
         AddLiteralSeparatorToNumberRector::class,
-        AddReturnTypeDeclarationFromYieldsRector::class => [
-            __DIR__ . '/Tests',
-        ],
+        PreferPHPUnitThisCallRector::class,
+        ReplaceTestAnnotationWithPrefixedFunctionRector::class,
     ]);
-};
